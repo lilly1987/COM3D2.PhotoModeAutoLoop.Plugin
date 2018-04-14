76ea10881ae4162fc50c375f3f088425f699b33b
lilly <lilly1987@naver.com>
2018-04-15 02:01:01 +0900

    SHIFT+CTR+L

--- COM3D2.PhotoModeAutoLoop.Plugin.cs
+++ COM3D2.PhotoModeAutoLoop.Plugin.cs
@@ -23,12 +23,13 @@ namespace COM3D2.PhotoModeAutLoop.Plugin
 	///
 	/// </remarks>
 	///=========================================================================
-	[PluginFilter( "COM3D2x64" ), PluginName("COM3D2.PhotoModeAutoLoop.Plugin"), PluginVersion( "1.0.0.0" )]
+	[PluginFilter( "COM3D2x64" ), PluginName("COM3D2.PhotoModeAutoLoop.Plugin"), PluginVersion( "1.0.0.0 edit by lilly 002" )]
 	public class PhotoModeAutoLoop: PluginBase
 	{
 		private bool isStudio = false;
+        private int isLoop = 0;
 		private float deltaTotal = 0;
-
+    
 		public void Awake()
 		{
 			try
@@ -48,26 +49,66 @@ namespace COM3D2.PhotoModeAutLoop.Plugin
 			{
 				if (isStudio)
 				{
-					deltaTotal += Time.deltaTime;
-					if (deltaTotal > 0.5f)
+                    //SHIFT+CTR+L
+					if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) &&
+						(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) &&
+						Input.GetKeyDown(KeyCode.L))
 					{
-						deltaTotal = 0;
-						List<Maid> maids = GameMain.Instance.CharacterMgr.GetStockMaidList();
-						if (maids != null)
-						{
-							foreach (Maid m in maids)
-							{
-								if (m != null && m.Visible)
-								{
-									Animation anm = m.GetAnimation();
-									if (anm != null)
-									{
-										anm.wrapMode = WrapMode.Loop;
-									}
-								}
-							}
-						}
+						isLoop += 1;
+                        if(isLoop>=2){
+                            isLoop=0;
+                        }
+                        Debug.Log("PhotoModeAutoLoop Input:"+isLoop);
 					}
+                    //if (isLoop){
+                        
+                        deltaTotal += Time.deltaTime;
+                        if (deltaTotal > 0.5f)
+                        {
+                            deltaTotal = 0;
+                            List<Maid> maids = GameMain.Instance.CharacterMgr.GetStockMaidList();
+                            if (maids != null)
+                            {
+                                foreach (Maid m in maids)
+                                {
+                                    if (m != null && m.Visible)
+                                    {
+                                        Animation anm = m.GetAnimation();
+                                        if (anm != null)
+                                        {
+                                            //스위치 방식으로 변경
+                                            switch (isLoop)
+                                            {
+                                                case 0:
+                                                    anm.wrapMode = WrapMode.Once;
+                                                    Console.WriteLine("Case 0 Once");
+                                                    break;
+                                                case 1:
+                                                    anm.wrapMode = WrapMode.Loop;
+                                                    Console.WriteLine("Case 1 Loop");
+                                                    break;
+                                                //case 2:
+                                                //    anm.wrapMode = WrapMode.PingPong;
+                                                //    Console.WriteLine("Case 2 PingPong");
+                                                //    break;
+                                                //case 3:
+                                                //    anm.wrapMode = WrapMode.Default;
+                                                //    Console.WriteLine("Case 3 Default");
+                                                //    break;
+                                                //case 4:
+                                                //    anm.wrapMode = WrapMode.ClampForever;
+                                                //    Console.WriteLine("Case 4 ClampForever");
+                                                //    break;
+                                                //default:
+                                                //    Console.WriteLine("Default case");
+                                                //    break;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    //}
 				}
 			}
 			catch (Exception e)
