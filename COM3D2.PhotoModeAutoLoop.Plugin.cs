bb92f108ae0dfc2449a59d59c546c8eb4eec99cb
lilly <lilly1987@naver.com>
2018-08-16 22:18:08 +0900

    Update COM3D2.PhotoModeAutoLoop.Plugin.cs
    미안 뭘 수정했는지 기억 안난다

--- COM3D2.PhotoModeAutoLoop.Plugin.cs
+++ COM3D2.PhotoModeAutoLoop.Plugin.cs
@@ -23,11 +23,11 @@ namespace COM3D2.PhotoModeAutLoop.Plugin
 	///
 	/// </remarks>
 	///=========================================================================
-	[PluginFilter( "COM3D2x64" ), PluginName("COM3D2.PhotoModeAutoLoop.Plugin"), PluginVersion( "1.0.0.0 edit by lilly 002" )]
+	[PluginFilter( "COM3D2x64" ), PluginName("COM3D2.PhotoModeAutoLoop.Plugin"), PluginVersion( "1.0.0.0 edit by lilly 003" )]
 	public class PhotoModeAutoLoop: PluginBase
 	{
-		private bool isStudio = false;
-        private int isLoop = 0;
+		//private bool isStudio = false;
+        private int isLoop = 2;
 		private float deltaTotal = 0;
     
 		public void Awake()
@@ -47,20 +47,25 @@ namespace COM3D2.PhotoModeAutLoop.Plugin
 		{
 			try
 			{
-				if (isStudio)
-				{
-                    //SHIFT+CTR+L
-					if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) &&
+				//if (isStudio)
+				//{
+                    //SHIFT+L
+					if (
 						(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) &&
-						Input.GetKeyDown(KeyCode.L))
+						Input.GetKeyDown(KeyCode.L)
+                        )
+                    //SHIFT+CTR+L
+					//if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) &&
+					//	(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) &&
+					//	Input.GetKeyDown(KeyCode.L))
 					{
 						isLoop += 1;
-                        if(isLoop>=2){
+                        if(isLoop>5){
                             isLoop=0;
                         }
                         Debug.Log("PhotoModeAutoLoop Input:"+isLoop);
 					}
-                    //if (isLoop){
+                    if (isLoop>0){
                         
                         deltaTotal += Time.deltaTime;
                         if (deltaTotal > 0.5f)
@@ -79,26 +84,26 @@ namespace COM3D2.PhotoModeAutLoop.Plugin
                                             //스위치 방식으로 변경
                                             switch (isLoop)
                                             {
-                                                case 0:
-                                                    anm.wrapMode = WrapMode.Once;
-                                                    Console.WriteLine("Case 0 Once");
-                                                    break;
                                                 case 1:
-                                                    anm.wrapMode = WrapMode.Loop;
-                                                    Console.WriteLine("Case 1 Loop");
+                                                    anm.wrapMode = WrapMode.Once;
+                                                    //Console.WriteLine("Case 0 Once");
                                                     break;
-                                                //case 2:
-                                                //    anm.wrapMode = WrapMode.PingPong;
-                                                //    Console.WriteLine("Case 2 PingPong");
-                                                //    break;
-                                                //case 3:
-                                                //    anm.wrapMode = WrapMode.Default;
-                                                //    Console.WriteLine("Case 3 Default");
-                                                //    break;
-                                                //case 4:
-                                                //    anm.wrapMode = WrapMode.ClampForever;
-                                                //    Console.WriteLine("Case 4 ClampForever");
-                                                //    break;
+                                                case 2:
+                                                    anm.wrapMode = WrapMode.Loop;
+                                                    //Console.WriteLine("Case 1 Loop");
+                                                    break;
+                                                case 3:
+                                                    anm.wrapMode = WrapMode.PingPong;
+                                                    //Console.WriteLine("Case 2 PingPong");
+                                                    break;
+                                                case 4:
+                                                    anm.wrapMode = WrapMode.Default;
+                                                    //Console.WriteLine("Case 3 Default");
+                                                    break;
+                                                case 5:
+                                                    anm.wrapMode = WrapMode.ClampForever;
+                                                    //Console.WriteLine("Case 4 ClampForever");
+                                                    break;
                                                 //default:
                                                 //    Console.WriteLine("Default case");
                                                 //    break;
@@ -108,8 +113,8 @@ namespace COM3D2.PhotoModeAutLoop.Plugin
                                 }
                             }
                         }
-                    //}
-				}
+				    }
+                //}
 			}
 			catch (Exception e)
 			{
@@ -119,24 +124,24 @@ namespace COM3D2.PhotoModeAutLoop.Plugin
 
 		private void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
 		{
-			try
-			{
-                if (scene.name == "ScenePhotoMode")
-				//if (scene.buildIndex == 27)
-				{
-					//スタジオモード
-					isStudio = true;
-				}
-				else
-				{
-					//スタジオモード以外
-					isStudio = false;
-				}
-			}
-			catch (Exception e)
-			{
-				Debug.LogError(e.ToString());
-			}
+			//try
+			//{
+            //    if (scene.name == "ScenePhotoMode")
+			//	//if (scene.buildIndex == 27)
+			//	{
+			//		//スタジオモード
+			//		isStudio = true;
+			//	}
+			//	else
+			//	{
+			//		//スタジオモード以外
+			//		isStudio = false;
+			//	}
+			//}
+			//catch (Exception e)
+			//{
+			//	Debug.LogError(e.ToString());
+			//}
 		}
 
 		public void OnGUI()
